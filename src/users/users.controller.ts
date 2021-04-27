5bbe686067b8fe4d9cac1e6c12714ae770074d3c
Michael Bentley <michaelbentley95@gmail.com>
2021-04-26 20:29:58 -0400

    Add validation and error handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -17,12 +17,20 @@ export class UsersController {
     }
 
     @ApiOkResponse({type: User})
+    @ApiNotFoundResponse()
     @Get(':id')
-    getUserById(@Param('id') id: string): User {
-        return this.usersService.findById(Number(id));
+    getUserById(@Param('id', ParseIntPipe) id: number): User {
+        const user = this.usersService.findById(id);
+        
+        if(!user){
+            throw new NotFoundException();
+        }
+
+        return user;
     }
 
     @ApiCreatedResponse({type: User})
+    @ApiBadRequestResponse()
     @Post()
     createUser(@Body() body: CreateUserDto): User {
         return this.usersService.createUser(body);
